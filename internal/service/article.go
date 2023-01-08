7c890c7ce066139712c131d32aa153ae85bd0c5e
huberyyang <yangcai@4399inc.com>
2023-01-08 21:28:46 +0800

    测试的接口

--- internal/service/article.go
+++ internal/service/article.go
@@ -31,10 +31,10 @@ type CreateArticleRequest struct {
 type UpdateArticleRequest struct {
 	ID            uint32 `form:"id" binding:"required,gte=1"`
 	Title         string `form:"title" binding:"max=100"`
-	Desc          string `form:"desc" binding:"min=10, max=100"`
-	Content       string `form:"content" binding:"min=10, max=65535"`
-	CoverImageUrl string `form:"cover_image_url" binding:"min=5,max=2000"`
-	State         uint8  `form:"state" binding:"required, oneof=0 1"`
+	Desc          string `form:"desc" binding:"max=100"`
+	Content       string `form:"content" binding:"max=65535"`
+	CoverImageUrl string `form:"cover_image_url"`
+	State         uint8  `form:"state" binding:"oneof=0 1"`
 	ModifiedBy    string `form:"modified_by" binding:"required,min=3,max=100"`
 }
 
