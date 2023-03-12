9036d6da771f5424827d79f558e357f854542814
huberyyang <yangcai@4399inc.com>
2023-03-12 17:25:17 +0800

    feat:优雅重启和停止

--- main.go
+++ main.go
@@ -68,7 +68,7 @@ func main() {
 	quit := make(chan os.Signal)
 	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
 	<-quit
-	log.Println("Shuting down server ...")
+	log.Println("Shutting down server ...")
 
 	timeout, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
